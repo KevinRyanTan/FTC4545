1d354584dd366a108253158ad7c860e856691272
Kevin Tan <kevin.ryan.tan@gmail.com>
2014-11-10 08:35:12 -0600

    Fixed encoders

--- moveRobotRearW.h
+++ moveRobotRearW.h
@@ -6,6 +6,10 @@ void moveRobotR(float BLspeed, float BRspeed, float BLrot, float BRrot)
 	float FRspeed = BLspeed;
 	float FRrot = BLrot;
 	float FLrot = BRrot;
+	bool FLdone = false;
+	bool BLdone = false;
+	bool FRdone = false;
+	bool BRdone = false;
 	while(!FLdone || !BLdone || !FRdone || !BRdone)
 	{
 		if(abs(nMotorEncoder[motorBL]) < abs(BLrot * 1120))

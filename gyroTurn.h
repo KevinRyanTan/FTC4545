974cd488ec113fb0cd9bc00648aac06a6ed3f741
Tank4545 <kevin.ryan.tan@gmail.com>
2014-11-17 22:11:56 -0600

    Fixed?

--- gyroTurn.h
+++ gyroTurn.h
@@ -9,7 +9,7 @@ void gyroTurn(bool left, int speed, int goal)
 		speed = speed * -1;
 	}
 	rotPos = HTGYROstartCal(HTGYRO);
-	while(position < goal)
+	while(abs(position) < abs(goal))
 	{
 		clearTimer(T3);
 		motor[motorFL] = speed;
@@ -23,57 +23,4 @@ void gyroTurn(bool left, int speed, int goal)
 		position = position + (rotVel * num);
 	}
 	stopMotors();
-	//int position = 0;
-	//float rotPos = 0.0;
-	//float rotVel = 0.0;
-	//bool done;
-	//if(left)
-	//{
-	//	speed = speed * -1;
-	//}
-	//	while(!done)
-	//	{
-	//		motor[motorFL] = speed;
-	//		motor[motorBL] = speed;
-	//		motor[motorFR] = -speed;
-	//		motor[motorBR] = -speed;
-	//		rotPos = HTGYROreadCal(HTGYRO);
-	//		rotVel = HTGYROreadRot(HTGYRO);
-	//		position = position + (rotVel  * .1);
-	//		if((position < goal + 5) && (position < goal + 5))
-	//			done = true;
-	//		wait1Msec(100);
-	//	}
-	//	stopMotors();
-	//	done = false;
-	//	while(!done)
-	//	{
-	//		while(position > goal)
-	//		{
-	//			clearTimer(T3);
-	//			motor[motorFL] = speed / -2;
-	//			motor[motorBL] = speed / -2;
-	//			motor[motorFR] = speed / 2;
-	//			motor[motorBR] = speed / 2;
-	//			rotPos = HTGYROreadCal(HTGYRO);
-	//			rotVel = HTGYROreadRot(HTGYRO);
-	//			rotVel = rotVel / time1[T3];
-	//			rotVel = rotVel * 1000;
-	//			position = position + rotVel;
-	//		}
-	//		stopMotors();
-	//		while(position < goal)
-	//		{
-	//			motor[motorFL] = speed / 2;
-	//			motor[motorBL] = speed / 2;
-	//			motor[motorFR] =  speed / -2;
-	//			motor[motorBR] = speed / -2;
-	//			rotPos = HTGYROreadCal(HTGYRO);
-	//			rotVel = HTGYROreadRot(HTGYRO);
-	//			position = position + (rotVel  * .1);
-	//		}
-	//		stopMotors();
-	//		if((position < goal + 0.5) && (position < goal + 0.5))
-	//			done = true;
-	//}
 }

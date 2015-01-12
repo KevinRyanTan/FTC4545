0b3346b0463eb9582b62d26361c084458bad4499
Tank4545 <kevin.ryan.tan@gmail.com>
2015-01-12 16:02:25 -0600

    Updated turning

--- gyroTurn.h
+++ gyroTurn.h
@@ -26,14 +26,16 @@ void gyroTurn(int speed, int goal)
 	int num = 0;
 	float rotVel = 0;
 	int position = 0;
+	wait1Msec(250);
 	while(abs(position) < abs(goal))
 	{
-		clearTimer(T3);
+		//clearTimer(T3);
+		position += HTGYROreadRot(HTGYRO) * (float) (20 / 1000.0);
 		motor[motorFL] = speed;
 		motor[motorBL] = speed;
 		motor[motorFR] = -speed;
 		motor[motorBR] = -speed;
-		wait1Msec(100);
+		/*wait1Msec(100);
 		num = time1(T3) / 1000;
 		rotPos = HTGYROreadCal(HTGYRO);
 		rotVel = HTGYROreadRot(HTGYRO);
@@ -41,8 +43,9 @@ void gyroTurn(int speed, int goal)
 		{
 			rotVel = rotVel * divisor;
 			position = position + rotVel / (1000 / time1(T3));
-		}
-		nxtDisplayCenteredTextLine(2, "position: %d", position);
+		}*/
+		//nxtDisplayCenteredTextLine(2, "position: %d", position);
+		wait1Msec(20);
 	}
 	//int new1 = abs(goal) - 5;
 	//int new2 = abs(goal) + 5
